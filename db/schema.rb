5454add6bed16e06e77f620214808dd34cf76c6d
rockydd <rockydd@gmail.com>
2014-06-06 06:44:16 +0800

    add roles_mask for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605222203) do
+ActiveRecord::Schema.define(version: 20140605224344) do
 
   create_table "games", force: true do |t|
     t.datetime "date"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140605222203) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "roles_mask"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
