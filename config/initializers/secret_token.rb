cdd6c0584e9e379ee69d5fc4cb1b5401d1041692
rockydd <rockydd@gmail.com>
2014-06-06 05:56:04 +0800

    world cup init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Worldcup::Application.config.secret_key_base = 'c0a3131c909a11a0387b444ef59c952e3beecf99bf691db7a307fa3539a0fe1d9fb22fd45191521935412039f5f3d9be830134e14cee50599345b641df9d8d01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Worldcup::Application.config.session_store :cookie_store, key: '_worldcup_session'
