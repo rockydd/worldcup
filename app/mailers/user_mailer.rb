bed2bb8ca7cd1a800debdf145bf5d079b35dd307
rockydd <rockydd@gmail.com>
2014-07-01 07:29:42 +0800

    gem unicorn

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,7 @@ class UserMailer < ActionMailer::Base
   def account_change(user, account_log)
     @user = user
     @account_log = account_log
-    if @user.email == 'rocky.dong@emc.com'
-      mail(to: 'worldcupdealer@gmail.com' || @user.email, subject: "world cup account change")
-    end
+    mail(to: 'worldcupdealer@gmail.com' || @user.email, subject: "world cup account change")
   end
 
 end
